63ca66b43e88ddae021dda908f1c20b26ac5c369
Victor Caruso <vic.caruso@gmail.com>
2022-03-23 16:46:04 -0700

    tenth exercise completed and test passing

--- exercise-10/exercise-10.sql
+++ exercise-10/exercise-10.sql
@@ -1,3 +1,30 @@
 -- CUBE of amount made per year, month, day, and film rating
 -- similar to exercise 9 but include the rating with joins
 -- replace ROLLUP with CUBE
+SELECT
+  rating,
+  EXTRACT (
+    YEAR
+    FROM
+      payment_date
+  ) y,
+  EXTRACT (
+    MONTH
+    FROM
+      payment_date
+  ) m,
+  EXTRACT (
+    DAY
+    FROM
+      payment_date
+  ) d,
+  SUM(amount)
+FROM
+  payment
+  INNER JOIN rental ON rental.rental_id = payment.rental_id
+  INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+  INNER JOIN film ON film.film_id = inventory.film_id
+GROUP BY
+  CUBE (rating, y, m, d)
+ORDER BY
+	rating, y, m, d
