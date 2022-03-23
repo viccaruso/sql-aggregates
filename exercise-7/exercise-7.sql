219bd4dee2564f80c42294d11d6dd2a2581f91d5
Victor Caruso <vic.caruso@gmail.com>
2022-03-23 16:07:59 -0700

    seventh exercise completed and test passing

--- exercise-7/exercise-7.sql
+++ exercise-7/exercise-7.sql
@@ -1,2 +1,14 @@
 -- customer ranking by total film time watched
 -- similar to exercise 6
+SELECT
+	email,
+    RANK() OVER (
+    ORDER BY
+      SUM(length) DESC
+  )
+FROM customer
+	INNER JOIN rental ON rental.customer_id = customer.customer_id
+    INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+    INNER JOIN film ON film.film_id = inventory.film_id
+GROUP BY
+	email;
