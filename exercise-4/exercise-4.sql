a1321e90cd0ffb0800f3733e74b6932b4d1238d2
Victor Caruso <vic.caruso@gmail.com>
2022-03-23 15:05:44 -0700

    fourth exercise completed and test passing

--- exercise-4/exercise-4.sql
+++ exercise-4/exercise-4.sql
@@ -1,2 +1,11 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT
+  name,
+  AVG(length)
+FROM
+  category
+  INNER JOIN film_category ON category.category_id = film_category.category_id
+  INNER JOIN film ON film_category.film_id = film.film_id
+GROUP BY
+  name;
