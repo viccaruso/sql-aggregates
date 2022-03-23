cde9ff718750fe5c3abbf6a8104b9a1c0c2a62c8
Victor Caruso <vic.caruso@gmail.com>
2022-03-23 15:29:14 -0700

    sixth exercise completed and test passing

--- exercise-6/exercise-6.test.js
+++ exercise-6/exercise-6.test.js
@@ -4,7 +4,7 @@ const pool = require('../lib/utils/pool');
 describe('exercise 6', () => {
   afterAll(() => pool.end());
 
-  it.skip('find customer ranking by total amount spent', async() => {
+  it('find customer ranking by total amount spent', async() => {
     const { rows } = await pool.query(fs.readFileSync(`${__dirname}/exercise-6.sql`, 'utf-8'));
     expect(rows).toEqual(require('./result.json'));
   });
